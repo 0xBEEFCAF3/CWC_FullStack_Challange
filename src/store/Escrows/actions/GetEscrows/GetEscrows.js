d6899cc56585a5cf4bf2477a0a5484cbdcb685bb
Armin Sabouri <armins@mit.edu>
2018-12-12 22:31:21 -0500

    add time to exp time of excrows

--- src/store/Escrows/actions/GetEscrows/GetEscrows.js
+++ src/store/Escrows/actions/GetEscrows/GetEscrows.js
@@ -29,70 +29,70 @@ export const getEscrows = () => {
       "asset":"btc",
       "amountTotal":1000000000,
       "amountTraded":750000000,
-      "expirationTime":1544443200
+      "expirationTime":1544943200
    },
    {
       "exchangeId":"kraken",
       "asset":"bch",
       "amountTotal":2000000000,
       "amountTraded":1158800000,
-      "expirationTime":1544482800
+      "expirationTime":1544982800
    },
    {
       "exchangeId":"bittrex",
       "asset":"ltc",
       "amountTotal":1000000000,
       "amountTraded":316200000,
-      "expirationTime":1544702400
+      "expirationTime":1545202400
    },
    {
       "exchangeId":"paymium",
       "asset":"ltc",
       "amountTotal":500000000,
       "amountTraded":62400000,
-      "expirationTime":1544788800
+      "expirationTime":1545288800
    },
    {
       "exchangeId":"poloniex",
       "asset":"btc",
       "amountTotal":1500000000,
       "amountTraded":251700000,
-      "expirationTime":1544886000
+      "expirationTime":1545386000
    },
    {
       "exchangeId":"bittrex",
       "asset":"bch",
       "amountTotal":500000000,
       "amountTraded":397100000,
-      "expirationTime":1544968800
+      "expirationTime":1545468800
    },
    {
       "exchangeId":"paymium",
       "asset":"btc",
       "amountTotal":1000000000,
       "amountTraded":500000000,
-      "expirationTime":1545652800
+      "expirationTime":1546152800
    },
    {
       "exchangeId":"kraken",
       "asset":"btc",
       "amountTotal":253000000,
       "amountTraded":56000000,
-      "expirationTime":1546300799
+      "expirationTime":1546800799
    },
    {
       "exchangeId":"bittrex",
       "asset":"ltc",
       "amountTotal":100000000,
       "amountTraded":23560000,
-      "expirationTime":1546621020
+      "expirationTime":1547121020
    },
    {
       "exchangeId":"poloniex",
       "asset":"bch",
       "amountTotal":60000000,
       "amountTraded":14657000,
-      "expirationTime":1547235275
+      "expirationTime":1547735275
    }];
     let results;
     if(getState().Escrows.Escrows.escrows.length == 0){
